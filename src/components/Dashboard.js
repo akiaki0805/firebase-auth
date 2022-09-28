c663db6af418b9b10e529c7ccfa52d0322a9f47a
aki <akiakitaku0805@gmail.com>
2022-09-28 17:09:32 +0900

    add userName to userProfile

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,10 +27,14 @@ export default function Dashboard() {
         <Card.Body>
           <h2 className='text-center mb-4'>あなたのプロフィール</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <strong>メールアドレス: </strong>{currentUser.email}
+
+          <strong>メールアドレス: </strong>{currentUser.email}<br/>
+          <strong>ユーザーネーム: </strong>
+
           <Link to = "/update-profile" className='btn btn-primary w-100 mt-3'>プロフィールを更新する</Link>
         </Card.Body>
       </Card>
+
       <div className='w-100 text-center mt-2'>
         <Button variant="link" onClick={handleLogout}>ログアウトする</Button>
       </div>
